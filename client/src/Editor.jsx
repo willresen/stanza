d64a61d720b32b81703229040e4bbf08ab2be41e
Will Resen <willresen@gmail.com>
2019-08-11 12:05:55 -0700

    Refactor and clean up code

--- client/src/Editor.jsx
+++ client/src/Editor.jsx
@@ -7,22 +7,17 @@ class Editor extends React.Component {
       text: ''
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({text: e.target.value});
   }
 
-  handleSubmit() {
-    console.log(new Set(this.state.text.replace(/\n/ig, ' ').split(' ')));
-  }
-
   render() {
     return (
       <div id="workspace">
         <div id="tools">
-          <button onClick={this.handleSubmit}>Analyze</button>
+          <button onClick={() => this.props.handleSubmit(this.state.text)}>Analyze</button>
         </div>
         <div id="editor">
           <textarea className="text" value={this.state.text} onChange={this.handleChange}></textarea>
