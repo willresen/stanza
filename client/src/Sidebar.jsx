6f98782ae00cbc5e1fb593c5992de54c041774f2
Will Resen <willresen@gmail.com>
2019-08-11 12:06:13 -0700

    Init

---
+++ client/src/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div id="sidebar">
+
+      </div>
+    );
+  };
+}
+
+export default Sidebar;
