45a215dc0a0be666a0aec7e085af8734bd4a2834
Will Resen <willresen@gmail.com>
2019-08-10 18:11:13 -0700

    Implement handleChange and handleSubmit functions

--- client/src/Editor.jsx
+++ client/src/Editor.jsx
@@ -4,17 +4,32 @@ class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      text: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
 
-
+  handleSubmit() {
+    console.log(new Set(this.state.text.replace(/\n/ig, ' ').split(' ')));
+  }
 
   render() {
     return (
-      <div></div>
+      <div id="workspace">
+        <div id="tools">
+          <button onClick={this.handleSubmit}>Analyze</button>
+        </div>
+        <div id="editor">
+          <textarea className="text" value={this.state.text} onChange={this.handleChange}></textarea>
+        </div>
+      </div>
     );
   };
-
 }
+
+export default Editor;
