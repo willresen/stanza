90cceef8811e2411521dfd851860dc4181947e1f
Will Resen <willresen@gmail.com>
2019-08-12 13:45:20 -0700

    Remove sort algorithm

--- client/src/OtherWords.jsx
+++ client/src/OtherWords.jsx
@@ -18,7 +18,7 @@ class OtherWords extends React.Component {
     return this.state.display && (
       <div id="other_words">
         <div id="rhymes">
-          <ul>{this.props.rhymes[0] && this.props.rhymes[0].sort().map(rhyme => <li>{rhyme}</li>)}</ul>
+          <ul>{this.props.rhymes[0] && this.props.rhymes[0].map(rhyme => <li>{rhyme}</li>)}</ul>
         </div>
         <div id="related">
           <ul>{this.props.related && this.props.related.map(word => <li>{word.word}</li>)}</ul>
