c0857c7e427eeb83eae8e95e531ffbfa30228c48
Will Resen <willresen@gmail.com>
2019-08-11 23:29:23 -0700

    Clean up code

--- client/src/Output.jsx
+++ client/src/Output.jsx
@@ -5,25 +5,27 @@ class Output extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      display: false,
       text: []
     }
   }
 
-  componentDidUpdate(prevProps) {
-    if (JSON.stringify(prevProps.text) !== JSON.stringify(this.props.text)) {
-      this.setState({text: this.props.text.replace(/\n/ig, '<br>').split('<br>')});
+  componentDidUpdate(prev) {
+    if (JSON.stringify(prev.text) !== JSON.stringify(this.props.text)) {
+      this.setState({text: this.props.text.replace(/\n/ig, '<br>').split('<br>'), display: true});
     }
   };
 
   render() {
     let rhymes = this.props.rhymes[0] || [];
-    return (<div id="output">{this.state.text.map(sentence => {
+    return this.state.display && (<div id="output">{this.state.text.map(sentence => {
       return (<p>{sentence.split(' ').map(word => {
         return (<span><Word
           handleClick={this.props.handleClick}
           word={word}
-          count={this.props.unique[word]}
-          highlight={rhymes.includes(word) || this.props.selected === word}
+          count={this.props.unique[word.replace(/[.,?!()]/, '')]}
+          highlight={rhymes.includes(word.replace(/[.,?!()]/, '')) ||
+            this.props.selected === word.replace(/[.,?!()]/, '')}
           rhymes={rhymes}
         /><span> </span></span>)
       })}</p>);
