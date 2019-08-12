ce515838828cfcd81883b37fb35e65b197c9cf6b
Will Resen <willresen@gmail.com>
2019-08-12 13:43:39 -0700

    Add sidebar checkboxes, refactor query logic to narrow results

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,33 +14,46 @@ class App extends React.Component {
       related: [],
       unique: {},
       selected: '',
+      showRepetition: true,
+      showCommonPhrases: false
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
     this.count = this.count.bind(this);
   };
 
   handleSubmit(text) {
-    let words = text.replace(/\n/ig, ' ').replace(/[.,?!()]/ig, '').split(' ');
+    let words = text.replace(/\n/ig, ' ').replace(/[^a-zA-Z ]/g, '').split(' ');
     let uniqueWords = this.count(words);
     this.setState({text: text, unique: uniqueWords});
   }
 
+  handleToggle(event) {
+    const toToggle = event.target.value;
+    this.setState({[toToggle]: !this.state[toToggle]})
+  }
+
   async handleClick(e) {
-    let word = e.target.innerText.replace(/[.,?!()]/gi, '');
+    let word = e.target.innerText.replace(/[^a-zA-Z ]/g, '');
     let rhymes = [];
     let related;
 
     await Promise.all([
       fetch(`https://api.datamuse.com/words?rel_rhy=${word}&max=1000`).then(result => result && result.json()),
-      fetch(`https://api.datamuse.com/words?rel_nry=${word}&max=1000`).then(result => result && result.json()),
+      fetch(`https://api.datamuse.com/words?arhy=1&max=5000&qe=sl&md=f&sl=${word}`).then(result => result && result.json()),
       fetch(`https://api.datamuse.com/words?rel_syn=${word}&max=1000`).then(result => result && result.json())
     ])
-      .then(([pureRhymes, slantRhymes, relatedWords]) => {
-        rhymes.push(pureRhymes.map(rhyme => rhyme.word).concat(slantRhymes.map(rhyme => rhyme.word)));
+      .then(([pefectRhymes, slantRhymes, relatedWords]) => {
+        rhymes.push(pefectRhymes.map(rhyme => rhyme.word).concat(slantRhymes.reduce((acc, next) => {
+          if (parseFloat(next.tags[0].split(':')[1]) >= 0.4) {
+            acc.push(next.word);
+          }
+          return acc;
+        }, [])));
         related = relatedWords;
       })
-    this.setState({ rhymes: rhymes, related: related, selected: word});
+    this.setState({ rhymes: rhymes, related: related, selected: word });
   }
 
   count(words) {
@@ -55,7 +68,9 @@ class App extends React.Component {
   render() {
     return (
       <div id="main">
-        <Sidebar />
+        <Sidebar handleToggle={this.handleToggle}
+          showRepetition={this.state.showRepetition}
+          showCommonPhrases={this.state.showCommonPhrases} />
         <Editor handleSubmit={this.handleSubmit} />
         <Output
           handleClick={this.handleClick}
@@ -63,8 +78,9 @@ class App extends React.Component {
           rhymes={this.state.rhymes}
           unique={this.state.unique}
           selected={this.state.selected}
+          showRepetition={this.state.showRepetition}
         />
-        <OtherWords rhymes={this.state.rhymes} related={this.state.related}/>
+        <OtherWords rhymes={this.state.rhymes} related={this.state.related} />
       </div>
     );
   };
