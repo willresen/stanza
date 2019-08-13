6bfc745f7a81c91e7b8a8388610a0fbf4281ae90
Will Resen <willresen@gmail.com>
2019-08-13 00:23:22 -0700

    Clean up code and add additional conditions for text coloring and highlighting

--- client/src/Word.jsx
+++ client/src/Word.jsx
@@ -5,25 +5,27 @@ class Word extends React.Component {
     super(props);
     this.state = {
       hovered: false
-    }
+    };
     this.handleMouseHover = this.handleMouseHover.bind(this);
-  }
+  };
 
   handleMouseHover(e) {
     this.setState({hovered: !this.state.hovered});
-  }
+  };
 
   render() {
     const color = this.props.count > 1 && this.props.showRepetition ? 'red' : 'white';
     const bgColor = this.props.highlight ? 'green' : 'transparent';
+    const wordClass = this.props.highlight ? 'focused_word' : 'word';
 
     return (<span
+      className={wordClass}
       onClick={this.props.handleClick}
       style={{ backgroundColor: bgColor, color: color }}
       onMouseEnter={this.handleMouseHover}
       onMouseLeave={this.handleMouseHover}>{this.props.word}
-    </span>)
-  }
-}
+    </span>);
+  };
+};
 
 export default Word;
