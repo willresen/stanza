119398a21a638dab3412ab4401bdb9730b3be87b
Will Resen <willresen@gmail.com>
2019-08-12 21:07:43 -0700

    Clean up code - fix indentation and spacing

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -1,62 +1,69 @@
 import React from 'react';
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  };
-
-  render() {
-    return (
-      <div id="sidebar">
-        <div id="save_load">
-          <div id="save">
-            <input type="text" placeholder="Title"></input>
-            <input type="text" placeholder="Author"></input><br/>
-            <button>Save</button>
-          </div>
-          <div id="load">
-            <input type="text" placeholder="Content ID"></input><br />
-            <button>Load</button>
-          </div>
-        </div>
-        <div id="controls">
-          <input type="checkbox"
-            value="showRepetition"
-            onChange={this.props.handleToggle}
-            checked={this.props.showRepetition}>
+const Sidebar = (props) => {
+  return (
+    <div id="sidebar">
+      <div id="logo">stanza</div><br/>
+      <div id="save_load">
+        <form id="save" onSubmit={props.handleSave}>
+          <input type="text"
+            name="title"
+            placeholder="Title"
+            onChange={props.handleChange}
+            required>
           </input>
-          <span>Mark repeated words</span><br />
-          <input type="checkbox"
-            value="highlightPerfectRhymes"
-            onChange={this.props.handleToggle}
-            checked={this.props.highlightPerfectRhymes}>
+          <input type="text"
+            name="author"
+            placeholder="Author"
+            onChange={props.handleChange}
+            required >
           </input>
-          <span>Highlight perfect rhymes</span><br />
-          <input type="checkbox"
-            value="highlightSlantRhymes"
-            onChange={this.props.handleToggle}
-            checked={this.props.highlightSlantRhymes}>
+          <button type="submit">Save</button><br/>
+        </form>
+        <form id="load">
+          <input type="text"
+            name="load_id"
+            placeholder="Content ID"
+            onChange={props.handleChange}
+            required>
           </input>
-          <span>Highlight slant rhymes</span><br /><br /> <br />
-          <input type="checkbox"
-            value="showCommonPhrases"
-            onChange={this.props.handleToggle}
-            checked={this.props.showCommonPhrases}>
-          </input>
-          <span>Show common phrases</span><br />
-          <input type="checkbox"
-            value="showSyllableCount"
-            onChange={this.props.handleToggle}
-            checked={this.props.showSyllableCount}>
-          </input>
-          <span>Show syllable count</span><br />
-        </div>
+          <button onClick={props.handleLoad}>Load</button><br/>
+        </form>
       </div>
-    );
-  };
-}
+      <div id="controls">
+        <input type="checkbox"
+          value="showRepetition"
+          onChange={props.handleToggle}
+          checked={props.showRepetition}>
+        </input>
+        <span>Mark repeated words</span><br/>
+        <input type="checkbox"
+          value="highlightPerfectRhymes"
+          onChange={props.handleToggle}
+          checked={props.highlightPerfectRhymes}>
+        </input>
+        <span>Highlight perfect rhymes</span><br/>
+        <input type="checkbox"
+          value="highlightSlantRhymes"
+          onChange={props.handleToggle}
+          checked={props.highlightSlantRhymes}>
+        </input>
+        <span>Highlight slant rhymes</span><br/><br/><br/>
+        <input type="checkbox"
+          value="showCommonPhrases"
+          onChange={props.handleToggle}
+          checked={props.showCommonPhrases}>
+        </input>
+        <span>Show common phrases</span><br/>
+        <input type="checkbox"
+          value="showSyllableCount"
+          onChange={props.handleToggle}
+          checked={props.showSyllableCount}>
+        </input>
+        <span>Show syllable count</span><br/>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
