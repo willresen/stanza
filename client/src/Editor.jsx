73d2c84fc5b591f37fd533a351d9ec3fc1e2a243
Will Resen <willresen@gmail.com>
2019-08-12 21:42:25 -0700

    Convert to stateless component

--- client/src/Editor.jsx
+++ client/src/Editor.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 
-class Editor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({text: e.target.value});
-  }
-
-  render() {
-    return (
-      <div id="workspace">
-        <div id="tools">
-          <button id="analyze" onClick={() => this.props.handleSubmit(this.state.text)}>Analyze</button>
-        </div>
-        <div id="editor">
-          <textarea className="text" value={this.state.text} onChange={this.handleChange} spellCheck="false"></textarea>
-        </div>
+const Editor = (props) => {
+  return (
+    <div id="workspace">
+      <div id="tools">
+        <button id="analyze" onClick={() => props.handleSubmit(props.editorText)}>Analyze</button>
       </div>
-    );
-  };
-}
+      <div id="editor">
+        <textarea className="text"
+          name="editorText"
+          value={props.editorText}
+          onChange={props.handleChange}
+          spellCheck="false">
+        </textarea>
+      </div>
+    </div>
+  );
+};
 
 export default Editor;
